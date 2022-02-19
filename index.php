ac38136e73e5180a15a070aa09370ae9d4b01b23
prod1gy87 <99636236+prod1gy87@users.noreply.github.com>
2022-02-19 11:29:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Сайт Виолетты';
+$name = 'Виолетка';
+$image = 'img/unknown-raccoon.svg';
+$email = 'vio-07@mail.ru';
+$phone = '+79876543210';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
